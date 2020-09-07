c11cd2c4a57400a2e72cb183f8f3f9012c228578
RachotiBiradar <44144000+RachotiBiradar@users.noreply.github.com>
2020-09-07 13:47:34 +0530

    defining functions of stats.h

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,76 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+#include<cmath>
+
+using namespace std;
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& arrayofNumbers) {
+
     //Implement statistics here
+    Statistics::Stats stat{nanf(""),nanf(""),nanf("")};
+    if((arrayofNumbers.size()>0) &&!IsNaNPresent(arrayofNumbers))
+    {
+    stat.average=average(arrayofNumbers);
+    stat.max=maxElement(arrayofNumbers);
+    stat.min=minElement(arrayofNumbers);
+   }
+    return stat;
 }
+
+float Statistics::minElement(const std::vector<float>& arrayofNumbers)
+{
+    float min=arrayofNumbers[0];
+    int iterator;
+       for(iterator=1;iterator<arrayofNumbers.size();iterator++)
+       {
+           if(arrayofNumbers[iterator]<min)
+               min=arrayofNumbers[iterator];
+       }
+    return min;
+}
+
+float Statistics::average(const std::vector<float>& arrayofNumbers)
+{
+    int iterator;float sum=0;
+    for(iterator=0;iterator<arrayofNumbers.size();iterator++)
+
+       {
+        sum=sum+arrayofNumbers[iterator];
+       }
+
+       return sum/arrayofNumbers.size();
+}
+
+float Statistics::maxElement(const std::vector<float>& arrayofNumbers)
+{
+    float max=arrayofNumbers[0];
+
+    int iterator;
+
+       for(iterator=1;iterator<arrayofNumbers.size();iterator++)
+
+       {
+
+           if(arrayofNumbers[iterator]>max)
+
+               max=arrayofNumbers[iterator];
+
+       }
+
+       return max;
+}
+bool Statistics::IsNaNPresent(const std::vector<float>& arrayofNumbers)
+{
+         int iterator;
+
+         for(iterator=0;iterator<arrayofNumbers.size();iterator++)
+
+         {
+
+             if(std::isnan(arrayofNumbers[iterator]))
+
+                 return true;
+
+         }
+
+    return false;
+ }
